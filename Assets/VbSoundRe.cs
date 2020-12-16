be6e954534f04d965cb89afb71deaf7e2ba935f5
euzl <euzl@khu.ac.kr>
2020-12-16 09:14:02 +0900

    feat: add virtual button

---
+++ Assets/VbSoundRe.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VbSoundRe : MonoBehaviour
+{
+    public GameObject vbBtnObj;
+    public AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("ReBtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(onButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(onButtonReleased);
+        
+        sound = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void onButtonPressed(VirtualButtonBehaviour vb)
+    {
+        sound.Play();
+        Debug.Log("BTN Pressed");
+    }
+
+    public void onButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("BTN Realesed");
+    }
+}
---
+++ Assets/vbSoundDo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbSoundDo : MonoBehaviour
+{
+    public GameObject vbBtnObj;
+    public AudioSource sound;
+    VirtualButtonBehaviour vbb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("DoBtn");
+        vbb = GetComponentsInChildren<VirtualButtonBehaviour>();
+        vbb[0].RegisterOnButtonPressed(onButtonPressed);
+        // vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(onButtonPressed);
+        // vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(onButtonReleased);
+
+        
+        sound = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onButtonPressed(VirtualButtonBehaviour vb)
+    {
+        sound.Play();
+        Debug.Log("BTN Pressed");
+    }
+
+    public void onButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("BTN Realesed");
+    }
+}
