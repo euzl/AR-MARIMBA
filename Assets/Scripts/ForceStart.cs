33f76f10159d35154a932c59463d4288af6ed8a3
euzl <euzl@khu.ac.kr>
2020-12-17 06:33:44 +0900

    feat: change Scene

---
+++ Assets/Scripts/ForceStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// 강제로 첫 Scene을 결정해주는 Script
+public class ForceStart : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name.CompareTo("StartScene") != 0)
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
